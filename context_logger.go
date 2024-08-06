07bed40b337d58ef30f5c870cca718dddc0fc0d6
Petr <vinokurovvin@list.ru>
2024-08-06 09:59:15 +0900

    RequestIDCtx

--- context_logger.go
+++ context_logger.go
@@ -8,6 +8,7 @@ import (
 type ctxKey string
 
 const loggerKey = ctxKey("logger")
+const requestIDKey = ctxKey("request_id")
 
 func ToCtx(ctx context.Context, logger *slog.Logger) context.Context {
 	return context.WithValue(ctx, loggerKey, logger)
@@ -21,4 +22,16 @@ func FromCtx(ctx context.Context) *slog.Logger {
 	}
 
 	return logger
+}
+
+func RequestIDToCtx(ctx context.Context, requestID string) context.Context {
+	return context.WithValue(ctx, requestIDKey, requestID)
+}
+
+func RequestIDFromCtx(ctx context.Context) string {
+	requestID, ok := ctx.Value(requestIDKey).(string)
+	if !ok {
+		return ""
+	}
+	return requestID
 }
--- context_logger_test.go
+++ context_logger_test.go
@@ -7,11 +7,24 @@ import (
 )
 
 func TestCtxLogger(t *testing.T) {
-	t.Skip()
 	l := slog.Default()
 	ctx := context.Background()
 	ctxWithLogger := ToCtx(ctx, l)
 	logger := FromCtx(ctxWithLogger)
 
 	logger.Info("test logger")
+}
+
+func TestRequestIDCtx(t *testing.T) {
+	ctx := context.Background()
+
+	sampleRequestID := "sample_request_id"
+
+	ctx = RequestIDToCtx(ctx, sampleRequestID)
+
+	requestID := RequestIDFromCtx(ctx)
+
+	if requestID != sampleRequestID {
+		t.Error("wrong request id")
+	}
 }
